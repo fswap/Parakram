c40c6c4ac9446855dc795c1079b216390d6c4374
MadhavShah <madhavshahsisodiya@gmail.com>
2019-02-08 00:27:14 +0530

    Added service and some minor changes

--- src/aws-exports.ts
+++ src/aws-exports.ts
@@ -5,7 +5,7 @@ const awsmobile = {
     "aws_appsync_graphqlEndpoint": "https://ge6yezluybamxauqsix2sdpqsa.appsync-api.us-east-1.amazonaws.com/graphql",
     "aws_appsync_region": "us-east-1",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-fe36qa5yhngqtjkvxybcrgozyy"
+    "aws_appsync_apiKey": "da2-bhlvbhn7sbbp5dtaznxdjssahq"
 };
 
 
